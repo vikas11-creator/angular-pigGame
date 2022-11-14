08947e4157f15c9a0e4a5c2b936b8d5214745468
vikas11-creator <82199478+vikas11-creator@users.noreply.github.com>
2022-11-14 20:58:30 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  ScoreP1: number = 0;
+  ScoreP2: number = 0;
+  showDice: boolean = false;
 }
