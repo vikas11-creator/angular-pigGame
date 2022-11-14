21a45caee0b23c8bf47d583ccb1dc6cf2cc578d0
vikas11-creator <82199478+vikas11-creator@users.noreply.github.com>
2022-11-15 00:21:20 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,10 @@ export class AppComponent  {
   ScoreP1: number = 0;
   ScoreP2: number = 0;
   showDice: boolean = false;
+  imgPath: string = '';
+  rollDice(){
+    const dice = Math.trunc(Math.random()*6)+1;
+    this.showDice = true;
+    this.imgPath = `../Asset/Images/dice-${dice}.png`;
+  }
 }
