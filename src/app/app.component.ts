ca89c4e46fcbcda3cd96178dbe1675f74c33f26e
vikas11-creator <82199478+vikas11-creator@users.noreply.github.com>
2023-07-18 20:29:23 +0530

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  ScoreP1: number = 0;
-  ScoreP2: number = 0;
-  showDice: boolean = false;
-  imgPath: string = '';
-  rollDice(){
-    const dice = Math.trunc(Math.random()*6)+1;
-    this.showDice = true;
-    this.imgPath = `../Asset/Images/dice-${dice}.png`;
-  }
+ 
 }
